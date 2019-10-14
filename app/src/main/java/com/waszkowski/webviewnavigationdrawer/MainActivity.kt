20fadf6028c1814642ccb4304b8c209eccc2424a
Derek Waszkowski <derekwaszkowski@gmail.com>
2019-10-14 19:13:58 -0400

    amazon and specific youtube time

---
+++ app/src/main/java/com/waszkowski/webviewnavigationdrawer/MainActivity.kt
@@ -0,0 +1,110 @@
+package com.waszkowski.webviewnavigationdrawer
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    internal var doubleBackToExitPressedOnce = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+
+
+        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        val toggle = ActionBarDrawerToggle(
+            this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer.setDrawerListener(toggle)
+        toggle.syncState()
+
+        val navigationView = findViewById<View>(R.id.nav_view) as NavigationView
+        navigationView.setNavigationItemSelectedListener(this)
+
+        //add this line to display menu1 when the activity is loaded
+        displaySelectedScreen(R.id.nav_menu1)
+    }
+
+
+    override fun onBackPressed() {
+        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START)
+        } else {
+            if (doubleBackToExitPressedOnce) {
+                super.onBackPressed()
+                return
+            }
+
+            this.doubleBackToExitPressedOnce = true
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+
+            Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+            // super.onBackPressed();
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        val id = item.itemId
+
+
+        return if (id == R.id.action_settings) {
+            true
+        } else super.onOptionsItemSelected(item)
+
+    }
+
+    private fun displaySelectedScreen(itemId: Int) {
+
+        //creating fragment object
+        var fragment: Fragment? = null
+
+        //initializing the fragment object which is selected
+        when (itemId) {
+            R.id.nav_menu1 -> fragment = Menu1()
+            R.id.nav_menu2 -> fragment = Menu2()
+            R.id.nav_menu3 -> fragment = Menu3()
+        }
+
+        //replacing the fragment
+        if (fragment != null) {
+            val ft = supportFragmentManager.beginTransaction()
+            ft.replace(R.id.content_frame, fragment)
+            ft.commit()
+        }
+
+        val drawer = findViewById<View>(R.id.drawer_layout) as DrawerLayout
+        drawer.closeDrawer(GravityCompat.START)
+    }
+
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        //calling the method displayselectedscreen and passing the id of selected menu
+        displaySelectedScreen(item.itemId)
+        //make this method blank
+        return true
+    }
+}
---
+++ app/src/main/java/com/waszkowski/webviewnavigationdrawer/Menu1.kt
@@ -0,0 +1,55 @@
+package com.waszkowski.webviewnavigationdrawer
+
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+
+class Menu1 : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        val v = inflater.inflate(R.layout.fragment_menu_1, container, false)
+        val mWebView = v.findViewById<View>(R.id.webview) as WebView
+        mWebView.loadUrl("https://codinghub.co.in.com")
+
+        // Enable Javascript
+        val webSettings = mWebView.settings
+        webSettings.javaScriptEnabled = true
+
+        // Force links and redirects to open in the WebView instead of in a browser
+        mWebView.webViewClient = WebViewClient()
+
+        mWebView.canGoBack()
+        mWebView.setOnKeyListener(View.OnKeyListener { v, keyCode, event ->
+            if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.action == MotionEvent.ACTION_UP
+                && mWebView.canGoBack()) {
+                mWebView.goBack()
+                return@OnKeyListener true
+            }
+            false
+        })
+
+
+
+        return v
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //you can set the title for your toolbar here for different fragments different titles
+        activity!!.title = "CodingHub.co.in"
+    }
+
+
+}
